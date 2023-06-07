d8c9eb1fdf8d4fd8a0da13801c7e7346214937f0
Arthur-Bamberg <bamberguisses@gmail.com>
2023-06-07 17:12:43 -0300

    feat: cleaning project

--- model/ChatGPTManager.js
+++
@@ -1,9 +0,0 @@
-class ChatGPTManager {
-    static async sendPrompt() {
-        //Needs to connect with ChatGPT API and send the user's prompt 
-    }   
-
-    static async newAnswer() {
-        //Needs to connect with ChatGPT API and send the user's prompt and return other response
-    }
-}
