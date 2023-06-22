fc5123b5e99c4aec7929bb0b26b2da12cb283d76
Arthur-Bamberg <bamberguisses@gmail.com>
2023-06-21 21:12:04 -0300

    feat: GPTConnector class created

---
+++ models/GPTConnector.class.ts
@@ -0,0 +1,67 @@
+import { Configuration, OpenAIApi, CreateCompletionRequest } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+class GPTConnector {
+    private readonly api: OpenAIApi;
+    private readonly delayMs: number;
+
+    constructor(delayMs: number = 1000) {
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+        this.api = new OpenAIApi(configuration);
+        this.delayMs = delayMs;
+    }
+
+    async sendPrompt(context: string, temperature: number, prompt: string): Promise<string | undefined> {
+        const gptPrompt = `Context: ${context}\nPrompt: ${prompt}\n`;
+
+        try {
+            const request: CreateCompletionRequest = {
+                model: 'text-davinci-003',
+                prompt: gptPrompt,
+                max_tokens: 100,
+                temperature,
+            };
+
+            const response = await this.makeRequestWithRetries(request, 3);
+
+            if (response.data.choices[0].text !== undefined) {
+                return response.data.choices[0].text.trim();
+            } else {
+                throw new Error('Unexpected response from OpenAI API: No choices found.');
+            }
+        } catch (error) {
+            console.error('Error connecting with ChatGPT', error);
+        }
+    }
+
+    private async makeRequestWithRetries(request: CreateCompletionRequest, maxRetries: number): Promise<any> {
+        let retries = 0;
+        while (retries < maxRetries) {
+            try {
+                if (retries > 0) {
+                    await this.delay(this.delayMs); // Add delay between retries
+                }
+                return await this.api.createCompletion(request);
+            } catch (error) {
+                console.error(`Error connecting with OpenAI API: ${error}`);
+            }
+        }
+        throw new Error(`Exceeded maximum retries (${maxRetries}) for API request.`);
+    }
+
+    private delay(ms: number): Promise<void> {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+}
+
+async function main() {
+    const gptConnector = new GPTConnector();
+    const gptResponse = await gptConnector.sendPrompt('Hello', 0.5, 'Hello, how are you?');
+
+    console.log(gptResponse);
+}
+
+main();
